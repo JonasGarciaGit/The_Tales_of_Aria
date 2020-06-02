c14c080847e7db38a6ffa050a9f20d7e25d299a2
JonasGarciaGit <jonasemprego1@gmail.com>
2020-06-01 21:11:53 -0300

    Cena de introducao adicionada

---
+++ Assets/Scripts/CloudsMoviment.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class CloudsMoviment : MonoBehaviour
+{
+    public Transform cloud;
+    public Transform[] position;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cloud.position = position[0].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moverNuvens();
+    }
+
+    void moverNuvens()
+    {
+        cloud.position = Vector3.MoveTowards(cloud.position, position[1].position, speed * Time.deltaTime);
+
+        if (cloud.position == position[1].position)
+        {
+            cloud.position = position[0].position;
+        }
+    }
+}
---
+++ Assets/Scripts/TextureFade.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureFade : MonoBehaviour
+{
+
+    SpriteRenderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+        Color c = rend.material.color;
+        c.a = 0f;
+        rend.material.color = c;
+
+        StartCoroutine("fadeOut");
+    }
+
+    IEnumerator FadeIn()
+    {
+        for(float f = 0.05f; f <= 1; f += 0.05f)
+        {
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    IEnumerator fadeOut()
+    {
+        for(float f = 1f; f >= -0.5f; f -= 0.05f)
+        {
+            Color c = rend.material.color;
+            c.a = f;
+            rend.material.color = c;
+            yield return new WaitForSeconds(0.2f);
+        }
+    }
+
+}
