1f342d79941726b3544bff14577ba6e72575820f
JonasGarciaGit <jonasemprego1@gmail.com>
2020-06-03 15:56:43 -0300

    Animação da porta abrindo pós Boss

---
+++ Assets/Scripts/OpenDoorAndShowUpCredits.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoorAndShowUpCredits : MonoBehaviour
+{
+
+    public GameObject door;
+    public GameObject darkAura;
+    public GameObject boss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && !boss.activeSelf)
+        {
+            StartCoroutine("openDoor");
+        }    
+    }
+
+    IEnumerator openDoor()
+    {
+        darkAura.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        door.GetComponent<Animator>().SetBool("Opening", true);
+    }
+}
